ae59c4456ae5bbd08d1a5ca4566bf3071acd673c
James Meegan <jmeegan2@owlmail.harford.edu>
2022-01-16 09:05:53 -0500

    first commit

---
+++ adjacent.js
@@ -0,0 +1,15 @@
+let arr = [ 1, 5, 7, -1, 5 ];
+
+function getPairsCount(arr, n, sum)
+{
+    let count = 0; // Initialize result
+
+    // Consider all possible pairs and check their sums
+    for (let i = 0; i < n; i++)
+        for (let j = i + 1; j < n; j++)
+            if (arr[i] + arr[j] == sum)
+                count++;
+
+    return count;
+}
+
